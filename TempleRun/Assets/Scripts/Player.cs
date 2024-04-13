3811912da10c5a7bc93c23ec119b063034a10ab3
Apostol-Alin <alin-constantin.apostol@s.unibuc.ro>
2024-04-13 17:25:19 +0300

    updated Player's money

---
+++ TempleRun/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int money = 0;
+
+    public void updateMoney(int ammount)
+    {
+        money += ammount;
+    }
+    public int getMoney()
+    {
+        return money;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<Coin>() != null)
+        {
+            money++;
+            Debug.Log(money);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
